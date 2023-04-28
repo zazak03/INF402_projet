aa7e7ce2e38081dfac526eae367f99ffe03fc6d1
JustAlternate <lololollololol890@gmail.com>
2023-04-28 22:08:46 +0200

    update des math, pour plus faire des factorielle 2468

--- norinori/bin/maths.ml
+++ norinori/bin/maths.ml
@@ -1,13 +1,3 @@
-
-
-
-let rec factorielle (n:int):int = 
-            match n with
-            | 0 | 1 -> 1
-            | n when n > 0 -> n*(factorielle (n-1))
-            | _ -> 1
-
-
-let k_parmis_n (k:int) (n:int):int = 
-            if (n < k) then 0
-            else ((factorielle n) / ((factorielle k) * (factorielle (n-k))))
+let 3_parmis_n (n:int) : int =
+            if (n < 3) then 0
+            else ((n)*(n-1)*(n-2))/6
