fe0adbca36031b6b5984194d8558dbcc7c8c54fb
JustAlternate <lololollololol890@gmail.com>
2023-04-28 16:44:48 +0200

    enfin fini

--- norinori/bin/main.ml
+++ norinori/bin/main.ml
@@ -2,19 +2,27 @@
 (*open Type_jeu*)
 open Lire_jeu
 open Ecrire_jeu
+open Visuel_resultat
 
 let () =
     if (Array.length Sys.argv != 2)
         then Printf.printf "\nusage: %s <nom>\n" Sys.argv.(0)
     else
+
     let nom:string = Sys.argv.(1) in (*le fichier est donné en argument*)
     let file_entree:string = (String.cat "INSTANCES/" (String.cat nom ".liz")) in
     let file_dimacs:string = (String.cat "DIMACS/" (String.cat nom ".dimacs")) in
+    let file_sortie:string = (String.cat "RESULTATS/" (String.cat nom ".res")) in
+    let file_visuel:string = (String.cat "RESULTATS/" (String.cat nom ".visu")) in
+
     (*Printf.printf "mon nom: %s\n" file;*)
     let mon_jeu = lire_fichier file_entree in (* on parse le fichier *)
 
     (*print_jeu mon_jeu;*) (*affichage de jeu*)
     sortie_dimacs file_dimacs mon_jeu;
+
+    faire_visuel mon_jeu file_sortie file_visuel;
+
     ()
     [@@warning "-8"]
 
--- norinori/bin/visuel_resultat.ml
+++ norinori/bin/visuel_resultat.ml
@@ -1,4 +1,36 @@
-let faire_visuel (j:jeu) (fichier:string) : unit =
+open Type_jeu
+
+type etat_automate_2 = Espace | Mot
+type memoire_automate_2 = out_channel*largeur*etat_automate_2*int
+
+let automate (((file:out_channel),(l:largeur),(etat:etat_automate_2),(avancement:int)):memoire_automate_2) (carac:char): memoire_automate_2=
+(*
+    | SPÉCIFICATION
+    | automate : a utiliser dans un String.fold_left, parse le fichier.
+    | - Profil : automate : mem_automate -> char -> mem_automate
+    | - Sémantique : (automate  mem_automate  char  mem_automate) : la fonction modifie mem_automate en fontion du caractere donnee et de son etat.
+    | - Exemple et propriétés :
+    |   (a) automate ((0, 0, 0, []), 15, Largeur) (';'): ((15, 0, 0, []), 0, Hauteur)
+    |   (b) automate ((15, 0, 0, []), 2, Hauteur) ('4'): ((15, 0, 0, []), 24, Hauteur)
+    | - Implémentation :
+*)
+    match carac,etat,((avancement+1) mod l) with
+    | ' ',_,_      ->     (file,l,Espace,avancement) 
+    | '0',Espace,_ ->     (file,l,Mot,avancement)
+    | '-',Espace,0 ->     Printf.fprintf file ".\n";
+                          (file,l,Mot,(avancement+1))
+    | '-',Espace,_ ->     Printf.fprintf file "."; 
+                          (file,l,Mot,(avancement+1))
+    |   _,Espace,0 ->     Printf.fprintf file "#\n"; 
+                          (file,l,Mot,(avancement+1))
+    |   _,Espace,_ ->     Printf.fprintf file "#"; 
+                          (file,l,Mot,(avancement+1))
+    |   _,_,_      ->     (file,l,Mot,avancement)
+
+
+
+
+let faire_visuel (j:jeu) (nom_entree:string) (nom_sortie:string) : unit =
     (*
     | SPÉCIFICATION
     | automate : permet douvrir un fichier, de le parser et retourner le jeu corespondant.
@@ -6,7 +38,12 @@ let faire_visuel (j:jeu) (fichier:string) : unit =
     | - Sémantique : (lire_fichier string) : récupère toutes les lignes puis applique un String.fold_left avec l'automate et retourne le jeu renvoyé.
     | - Implémentation :
     *)
-    let lines_array = Array.to_list (Arg.read_arg fichier) in (*on recupere les lignes*)
-    let lines =  String.concat "" lines_array in (*on concatene toutes les lignes*)
-
-
+    let (l,_,_,_) = j in 
+    let lines_array = (Arg.read_arg nom_entree) in (*on recupere les lignes*)
+    let solution = lines_array.(1) in (*on concatene toutes les lignes*)
+    let file:out_channel = open_out nom_sortie in
+    Printf.fprintf file "\n";
+    let _ = (String.fold_left automate (file, l, Espace, 0) solution) in
+    Printf.fprintf file "\n";
+    close_out file;
+    ()
